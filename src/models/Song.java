4a627ed59c56d7e8a308dea0db6f729e23d7aa61
emproy91 <emproy91@gmail.com>
2022-06-03 22:25:38 -0500

    Array Songs library done

--- src/models/Song.java
+++ src/models/Song.java
@@ -1,5 +1,9 @@
 package models;
-
+/**
+ * Represents the information of a song.
+ * @author Mauricio Pérez Rojas.
+ * @author Luis Javier Rincon.
+ */
 import java.util.Date;
 
 public class Song {
@@ -12,6 +16,23 @@ public class Song {
     private String cover;
     private String description;
 
+    /**
+     * new void instance of the Song class.
+     */
+    public Song () {
+
+    }
+
+    /**
+     * Create a new instance of the Song class.
+     * @param iD Integer to identify the song.
+     * @param title Name of song.
+     * @param date Date release of song.
+     * @param duration Song length in seconds.
+     * @param genre Musical genre of the song.
+     * @param cover Song cover path.
+     * @param description Song description.
+     */
     public Song(int iD, String title, Date date, int duration, String genre, String cover, String description) {
         this.iD = nID();
         this.title = title;
@@ -22,20 +43,31 @@ public class Song {
         this.description = description;
 
     }
+
+    /**
+     * Create a new random integer to identify the song.
+     * @return A new _iD.
+     */
     public int nID() {
         int _iD = (int) (Math.random() * 999);
+        // Generate and save a new random id.
         return _iD;
     }
+
+    /**
+     * Create a new character string with the parameters of the class.
+     * @return String array with data to show.
+     */
     @Override
     public String toString() {
         return "Song{" +
-                " iD=" + iD +
-                ", title='" + title + '\'' +
-                ", date=" + date +
-                ", duration=" + duration +
-                ", genre='" + genre +"',"+ "\n"+
-                "      cover='" + cover + "'," + "\n"+
-                "      description='" + description + '\'' + "\n"+
+                " iD: " + iD +
+                ", title: '" + title + '\'' +
+                ", date: " + date +
+                ", duration: " + duration +
+                ", genre: '" + genre +"',"+ "\n" +
+                "      cover: '" + cover + "'," + "\n" +
+                "      description: '" + description + '\'' + "\n" +
                 "    }";
     }
 
@@ -43,4 +75,59 @@ public class Song {
      * Access methods.
      * Setters and getters.
      */
+    public int getiD() {
+        return iD;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
